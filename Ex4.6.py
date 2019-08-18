07cde99adee572a38e957b0e757b5864a99a7650
Franky <mjenkinslindsay@gmail.com>
2019-08-18 18:49:56 -0500

    Update Ex4.6.py
    Remove "Overtime" and "Regular" print statements

--- Ex4.6.py
+++ Ex4.6.py
@@ -2,10 +2,10 @@ def computepay(hours, rate):
     if hours > 40.0:
         overtime_rate = rate * 1.5
         pay = (hours - 40) * overtime_rate + (rate * 40)
-        #print("Overtime")
+
     else:
         pay = hours * rate
-        #print("Regular")
+
     return pay
 hours = input('How many hours did you work?: ')
 rate = input('What is your hourly rate?: ')
