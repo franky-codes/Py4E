d116a243c3abd70e02bb61a2c6867462c495eb83
franky-codes <42191840+franky-codes@users.noreply.github.com>
2019-11-15 10:06:10 -0600

    Grade calculator rewrite
    Rewrote grade calculator. Made a bit neater, removed unneccessary try/excepts

--- Ex3.3.py
+++ Ex3.3.py
@@ -1,27 +1,18 @@
-raw_score = input('Enter score: ')
+score = input('Enter score: ')
+num_score = float(score)
 
-try:
-    score = float(raw_score)
-except:
-    print('Score must be entered as a numeric value')
-    quit()
+if num_score > 1.0 or num_score < 0.0:
+  print('Score must be between 0.0 and 1.0')
+  quit()
+elif num_score >= 0.9:
+  grade = 'A'
+elif num_score >= 0.8:
+  grade = 'B'
+elif num_score >= 0.7:
+  grade = 'C'
+elif num_score >= 0.6:
+  grade = 'D'
+elif num_score < 0.6:
+  grade = 'F'
 
-try:
-    score >= 0.0 and score <= 1.0
-except:
-    print('Score must be between 0.0 and 1.0')
-    quit()
-
-# if score >= 0.0 and 1.0 >= score: DO THIS AS A TRY/EXCEPT
-
-if score >= 0.9:
-    print('A')
-elif score >= 0.8:
-    print('B')
-elif score >= 0.7:
-    print('C')
-elif score >= 0.6:
-    print('D')
-elif score < 0.6:
-    print('F')
-# else:
+print('Grade is ',grade)
