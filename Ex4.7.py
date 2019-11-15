3a0ec48cabe9e483da0c6d6b53696c55f0cd4f77
franky-codes <42191840+franky-codes@users.noreply.github.com>
2019-11-15 12:54:46 -0600

    Rewrote Ex4.7
    Wrote a cleaner version of previous solution.

--- Ex4.7.py
+++ Ex4.7.py
@@ -26,3 +26,28 @@ except:
     quit()
 
 computegrade(raw_score)
+
+##############I REWROTE THE SAME PROGRAM####################
+
+score = input('Enter score: ')
+try:
+  num_score = float(score)
+except:
+  print('Score must be numeric')
+  quit()
+def computegrade(score):
+  if score > 1.0 or score < 0.0:
+    print('Score must be between 0.0 and 1.0')
+    quit()
+  elif score >= 0.9:
+    print('A')
+  elif score >= 0.8:
+    print('B')
+  elif score >= 0.7:
+    print('C')
+  elif score >= 0.6:
+    print('D')
+  elif score < 0.6:
+    print('F')
+
+computegrade(num_score)
