42317efe02b8923fcfda4ca3ec1c62a7ea5c9f8b
Franky <mjenkinslindsay@gmail.com>
2020-03-26 10:54:50 -0500

    Revert "Update Ex4.7.py"
    This reverts commit c053d7e0c88e7df9d83246433e05a4587bdad41b.

--- Ex4.7.py
+++ Ex4.7.py
@@ -1,6 +1,28 @@
-def calculategrade(score):
-    try:
-        score = float(score)
-        if score < 0.0 or score > 1.0:
-            return('Bad score')
-        elif 
+# computegrade function takes a float and outputs a letter grade
+def computegrade(score):
+    if score >= 0.9:
+        print('A')
+    elif score >= 0.8:
+        print('B')
+    elif score >= 0.7:
+        print('C')
+    elif score >= 0.6:
+        print('D')
+    elif score < 0.6:
+        print('F')
+
+raw_score = input('Enter score: ')
+
+try:
+    score = float(raw_score) # is this variable global?
+except:
+    print('Score must be entered as a numeric value')
+    quit()
+
+try:
+    score >= 0.0 and score <= 1.0
+except:
+    print('Score must be between 0.0 and 1.0')
+    quit()
+
+computegrade(raw_score)
